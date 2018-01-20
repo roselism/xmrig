777d8fe5d419a4eeec335f58e5c4d7a710134eac
王镇 <HyperSimon@users.noreply.github.com>
2018-01-21 01:14:37 +0800

    Update mining.sh
    新增 build

--- mining.sh
+++ mining.sh
@@ -1,1 +1,13 @@
-./xmrig -o pool.monero.hashvault.pro:5555 -t 5 -u 49RmeCdfC6sLffqcmtyfQBDhPEh3RMK3iEHjgbbTsZgPMD9ZxEtEATC445qtgaxEpSaTELjwJBxEKDwKuoHd2fB7ViYojy3 -p hyperworker -k
+#!/bin/bash
+
+# install 
+brew install cmake libuv libmicrohttpd ;
+git clone https://github.com/xmrig/xmrig.git ;
+cd xmrig ;
+mkdir build ;
+cd build ;
+cmake .. ;
+make ;
+
+# start mining
+./xmrig -o pool.monero.hashvault.pro:5555 -t 5 -u 49RmeCdfC6sLffqcmtyfQBDhPEh3RMK3iEHjgbbTsZgPMD9ZxEtEATC445qtgaxEpSaTELjwJBxEKDwKuoHd2fB7ViYojy3 -p hyperworker -k     
